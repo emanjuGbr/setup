629b492690cd4d1c85f4b4c3b98ea3dde37bb627
chrizzTs <c.mahlich@gmail.com>
2018-08-16 17:40:43 +0200

    Changed container name from service to setup and added if to check if running on Mac Os to use different sed command

--- create_domain.sh
+++ create_domain.sh
@@ -10,11 +10,20 @@ echo "domain: $1"
 echo -n "1.) setup new nginx config."
 
 cp nginx/conf.d/odoo.template nginx/conf.d/$1.conf
-sed -i "s/<replace_sn>/$1/g" nginx/conf.d/$1.conf
-docker exec -ti service_nginx_1 nginx -s reload
+
+if [ "$(uname)" == "Darwin" ]; 
+then
+	echo "Running on Mac OS"
+	sed "s/<replace_sn>/$1/g" nginx/conf.d/$1.conf
+else 
+	echo "Running not on Mac OS"
+	sed -i "s/<replace_sn>/$1/g" nginx/conf.d/$1.conf
+fi
+
+docker exec -ti setup_nginx_1 nginx -s reload
 echo  " ok"
 echo -n "2.) create certificate."
-docker run --rm -ti --network service_network --ip 10.18.77.5 -v /etc/letsencrypt:/etc/letsencrypt -v /tmp:/var/log/letsencrypt \
+docker run --rm -ti --network setup_network --ip 10.18.77.5 -v /etc/letsencrypt:/etc/letsencrypt -v /tmp:/var/log/letsencrypt \
         certbot/certbot certonly \
         --agree-tos \
         -m mahlich@emanju.de \
@@ -23,6 +32,12 @@ docker run --rm -ti --network service_network --ip 10.18.77.5 -v /etc/letsencryp
         -d $1
 echo " ok"
 echo -n "3.) activate tls for $1."
-sed -i "s/#//g" nginx/conf.d/$1.conf
-docker exec -ti service_nginx_1 nginx -s reload
+
+if [ "$(uname)" == "Darwin" ]; 
+then
+	sed "s/#//g" nginx/conf.d/$1.conf
+else
+	sed -i "s/#//g" nginx/conf.d/$1.conf
+fi
+	docker exec -ti setup_nginx_1 nginx -s reload
 echo " ok"
