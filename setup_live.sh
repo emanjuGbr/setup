1f9440ebeb50e32cc91629b454c2974bd5335e75
Christian Mahlich <c.mahlich@gmail.com>
2018-08-24 16:42:50 +0200

    Change filestore to correct directory

--- setup_live.sh
+++ setup_live.sh
@@ -14,7 +14,7 @@ if [ "$STORAGEBOX_PATH" == "" ]; then
 fi
 echo "STORAGEBOX=$STORAGEBOX_PATH" >> .env
 # change directory access rights
-docker run --rm -t --user root -v $(pwd)/odoo/:/odoo -v $STORAGEBOX_PATH/filestore:/odoo/filestore odoo:11.0 chown odoo: /odoo -R
+docker run --rm -t --user root -v $(pwd)/odoo/:/odoo -v $STORAGEBOX_PATH/odoo/filestore/:/odoo/filestore odoo:11.0 chown odoo: /odoo -R
 docker run --rm -t --user root -v $(pwd)/postgres/data:/postgres postgres:9.6-alpine chown postgres: /postgres -R
 docker run --rm -t --user root -v $(pwd)/nginx/conf.d:/etc/nginx/conf.d nginx:alpine chown nginx: /etc/nginx/conf.d -R
 
