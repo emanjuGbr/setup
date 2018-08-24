2b01dbddc0220632796b5bbc29b8f2684ecbf74f
Christian Mahlich <c.mahlich@gmail.com>
2018-08-24 16:54:02 +0200

    FIX Storagebox directory

--- setup_live.sh
+++ setup_live.sh
@@ -10,11 +10,11 @@ sed "s/<replace>/$PW/g" odoo/odoo.conf.template > odoo/odoo.conf
 sed "s/<replace>/$PW/g" odoo/odoo-config.conf.template > odoo/odoo-config.conf
 echo -e "PGPASSWORD=$PW\nPOSTGRES_PASSWORD=$PW" > .env
 if [ "$STORAGEBOX_PATH" == "" ]; then
-	STORAGEBOX_PATH=$(pwd);
+	STORAGEBOX_PATH=$(pwd)/odoo;
 fi
 echo "STORAGEBOX=$STORAGEBOX_PATH" >> .env
 # change directory access rights
-docker run --rm -t --user root -v $(pwd)/odoo/:/odoo -v $STORAGEBOX_PATH/odoo/filestore/:/odoo/filestore odoo:11.0 chown odoo: /odoo -R
+docker run --rm -t --user root -v $(pwd)/odoo/:/odoo -v $STORAGEBOX_PATH/filestore/:/odoo/filestore odoo:11.0 chown odoo: /odoo -R
 docker run --rm -t --user root -v $(pwd)/postgres/data:/postgres postgres:9.6-alpine chown postgres: /postgres -R
 docker run --rm -t --user root -v $(pwd)/nginx/conf.d:/etc/nginx/conf.d nginx:alpine chown nginx: /etc/nginx/conf.d -R
 
