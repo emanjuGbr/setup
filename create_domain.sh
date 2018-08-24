2d7e0e0743b0b8c4c7862a4b203c1c78abbea368
Grohmann, Michael (Intero Technologies) <m.grohmann@intero-technologies.de>
2018-08-24 14:22:26 +0200

    [IMP] filestore/storagebox setup + config odoo instance

--- create_domain.sh
+++ create_domain.sh
@@ -7,23 +7,26 @@ then
 fi
 echo "domain: $1"
 
+if [ "$(uname)" == "Darwin" ];
+then
+	echo "Running on Mac OS"
+	sed="sed"
+else
+	echo "Running not on Mac OS"
+	sed="sed -i"
+fi
+
+
 echo -n "1.) setup new nginx config."
 
 cp nginx/conf.d/odoo.template nginx/conf.d/$1.conf
+$sed "s/<replace_sn>/$1/g" nginx/conf.d/$1.conf
 
-if [ "$(uname)" == "Darwin" ]; 
-then
-	echo "Running on Mac OS"
-	sed "s/<replace_sn>/$1/g" nginx/conf.d/$1.conf
-else 
-	echo "Running not on Mac OS"
-	sed -i "s/<replace_sn>/$1/g" nginx/conf.d/$1.conf
-fi
 
 docker exec -ti setup_nginx_1 nginx -s reload
 echo  " ok"
 echo -n "2.) create certificate."
-docker run --rm -ti --network setup_network --ip 10.18.77.5 -v /etc/letsencrypt:/etc/letsencrypt -v /tmp:/var/log/letsencrypt \
+docker run --rm -ti --network setup_network --ip 10.18.77.5 -v $(pwd)/etc/letsencrypt:/etc/letsencrypt -v /tmp:/var/log/letsencrypt \
         certbot/certbot certonly \
         --agree-tos \
         -m mahlich@emanju.de \
@@ -33,11 +36,10 @@ docker run --rm -ti --network setup_network --ip 10.18.77.5 -v /etc/letsencrypt:
 echo " ok"
 echo -n "3.) activate tls for $1."
 
-if [ "$(uname)" == "Darwin" ]; 
-then
-	sed "s/#//g" nginx/conf.d/$1.conf
-else
-	sed -i "s/#//g" nginx/conf.d/$1.conf
-fi
-	docker exec -ti setup_nginx_1 nginx -s reload
+$sed "s/#//g" nginx/conf.d/$1.conf
 echo " ok"
+if [ "$1" == "config.emanju.de" ];
+then
+	$sed "s/odoo:/odoo-config:/g" nginx/conf.d/$1.conf
+fi
+docker exec -ti setup_nginx_1 nginx -s reload
--- setup_live.sh
+++ setup_live.sh
@@ -7,10 +7,14 @@ genpasswd() {
 # generate odoo master & postgres password
 PW="$(genpasswd 32)"
 sed "s/<replace>/$PW/g" odoo/odoo.conf.template > odoo/odoo.conf
+sed "s/<replace>/$PW/g" odoo/odoo-config.conf.template > odoo/odoo-config.conf
 echo -e "PGPASSWORD=$PW\nPOSTGRES_PASSWORD=$PW" > .env
-
+if [ "$STORAGEBOX_PATH" == "" ]; then
+	STORAGEBOX_PATH=$(pwd);
+fi
+echo "STORAGEBOX=$STORAGEBOX_PATH" >> .env
 # change directory access rights
-docker run --rm -t --user root -v $(pwd)/odoo/:/odoo odoo:11.0 chown odoo: /odoo -R
+docker run --rm -t --user root -v $(pwd)/odoo/:/odoo -v $STORAGEBOX_PATH/filestore:/odoo/filestore odoo:11.0 chown odoo: /odoo -R
 docker run --rm -t --user root -v $(pwd)/postgres/data:/postgres postgres:9.6-alpine chown postgres: /postgres -R
 docker run --rm -t --user root -v $(pwd)/nginx/conf.d:/etc/nginx/conf.d nginx:alpine chown nginx: /etc/nginx/conf.d -R
 
